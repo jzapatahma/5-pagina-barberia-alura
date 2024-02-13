bca45ffc05afecc8a8045c865ac7ae2267c84336
Jovany <157656628+jzapatahma@users.noreply.github.com>
2024-02-13 09:47:58 -0500

    Iniciacion proyecto a terminar responsive

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function mostrar(){
+    document.getElementById('sliding_menu').classList.add('open');
+}
+function ocultar(){
+    document.getElementById('sliding_menu').classList.remove('open');
+}
